f3cebb41dbfa01785032d40cf2dcff52483de464
Martin Lindhe <martin@ubique.se>
2016-01-29 12:15:27 +0100

    colorized output when --pretty is enabled; based on https://github.com/miolini/jsonf/blob/master/jsonf.go

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"regexp"
 
 	"gopkg.in/alecthomas/kingpin.v2"
 )
@@ -34,9 +35,48 @@ func main() {
 		if err != nil {
 			fmt.Println("ERROR:", err)
 		}
+
+		if b, err = highlight(b); err != nil {
+			fmt.Println("ERROR highlight:", err)
+		}
 		fmt.Printf(string(b))
 
 	} else {
 		fmt.Println("OK:", *inFile)
 	}
+}
+
+type rule struct {
+	Expr    string
+	Replace string
+}
+
+var rules = []rule{{"", ""},
+	{`(?m)(\"[^\"]+\"):`, "\033[36m$1\033[39m:"},
+	{`(?m)(^\s*[{}\]]{1}[,]*$|: [{\[])`, "\033[33m$1\033[39m"},
+	{`: (\"[^\"]+\")`, ": \033[31m$1\033[39m$2"},
+	{`(?m): ([\d][\d\.e+]*)([,]*)$`, ": \033[33m$1\033[39m"},
+	{`(?m)(^\s+(?:[\d][\d\.e+]*|true|false|null))([,]*)$`, "\033[35m$1\033[39m$2"},
+	{`(?::) ((?:true|false|null))`, ": \033[31m$1\033[39m"},
+	{`(?m)^(true|false|null)$`, "\033[31m$1\033[39m"},
+}
+
+func highlight(data []byte) ([]byte, error) {
+	var (
+		re  *regexp.Regexp
+		err error
+	)
+	for _, rule := range rules {
+		if rule.Expr == "" {
+			continue
+		}
+
+		re, err = regexp.Compile(rule.Expr)
+		if err != nil {
+			return nil, err
+		}
+		data = re.ReplaceAll(data, []byte(rule.Replace))
+	}
+
+	return data, nil
 }
