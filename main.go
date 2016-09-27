9eafb7d5130cc3846b277ef68ac50c800b324c8b
Martin Lindhe <martin-commit@ubique.se>
2016-09-27 15:10:01 +0200

    also accept data from stdin

--- main.go
+++ main.go
@@ -7,12 +7,13 @@ import (
 	"os"
 	"regexp"
 
+	termutil "github.com/andrew-d/go-termutil"
 	"github.com/mattn/go-isatty"
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
 var (
-	inFile = kingpin.Arg("file", "JSON file.").Required().ExistingFile()
+	inFile = kingpin.Arg("file", "JSON file.").String()
 	pretty = kingpin.Flag("pretty", "Pretty print result.").Short('p').Bool()
 	quiet  = kingpin.Flag("quiet", "Don't output on success.").Short('q').Bool()
 )
@@ -23,10 +24,14 @@ func main() {
 	kingpin.CommandLine.HelpFlag.Short('h')
 	kingpin.Parse()
 
-	data, _ := ioutil.ReadFile(*inFile)
+	data, err := readPipeOrFile(*inFile)
+	if err != nil {
+		fmt.Println("error:", err)
+		os.Exit(1)
+	}
 
 	var f interface{}
-	err := json.Unmarshal(data, &f)
+	err = json.Unmarshal(data, &f)
 	if err != nil {
 		fmt.Println("ERROR:", *inFile, err)
 		os.Exit(1)
@@ -90,4 +95,15 @@ func highlight(data []byte) ([]byte, error) {
 	}
 
 	return data, nil
+}
+
+// readPipeOrFile reads from stdin if pipe exists, else from provided file
+func readPipeOrFile(fileName string) ([]byte, error) {
+	if !termutil.Isatty(os.Stdin.Fd()) {
+		return ioutil.ReadAll(os.Stdin)
+	}
+	if fileName == "" {
+		return nil, fmt.Errorf("no piped data and no file provided")
+	}
+	return ioutil.ReadFile(fileName)
 }
